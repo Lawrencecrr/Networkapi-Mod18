a75f17fcaaa6d9ab6ab9740d8c62fa8f637eda7e
Lawrencecrr <lawrenceschoolmail@gmail.com>
2022-10-06 15:30:56 -0400

    added to index.js

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -11,11 +11,24 @@ const userController = {
           });
 
     },
-    getUserById(req, res) {
-
+    getUserById({params}, res) {
+      User.findOne({_id: params.id})
+        .then(dbUserData => {
+          if(!dbUserData) {
+            res.status(404).json({ message: 'no user found with this id'});
+            return;
+          }
+          res.json(dbUserData)
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(400).json(err);
+        });
     },
-    createUser(req, res) {
-
+    createUser({body}, res) {
+      User.create(body)
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => res.status(400).json(err));
     },
     updateUser(req, res) {
 
