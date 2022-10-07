5123a4dff14df4774bf09e62d486dc29eddd30fe
Lawrencecrr <lawrenceschoolmail@gmail.com>
2022-10-07 18:00:11 -0400

    Thought Controller.js

---
+++ routes/thought-routes.js
@@ -0,0 +1,4 @@
+const router = require('express').Router;
+
+
+const {getAllThoughts, getThoughtById, createThought, updateThought, deleteThought, addReaction, deleteReaction,} = require('../controllers/thought-controller');
