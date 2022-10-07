142c5d0f66e6a3e94c0cc56ef7bf60eff2e36fc6
Lawrencecrr <lawrenceschoolmail@gmail.com>
2022-10-07 18:05:35 -0400

    thought-route finished

--- routes/thought-routes.js
+++ routes/thought-routes.js
@@ -2,3 +2,19 @@ const router = require('express').Router;
 
 
 const {getAllThoughts, getThoughtById, createThought, updateThought, deleteThought, addReaction, deleteReaction,} = require('../controllers/thought-controller');
+
+
+router.router('/').get(getAllThoughts).post(createThought);
+
+
+router
+.route('/:id')
+.get(getThoughtById)
+.put(updateThought)
+.delete(deleteThought);
+
+
+router.route("/:thoughtId/reactions").post(addReaction).delete(deleteReaction);
+
+
+module.exports = router;
